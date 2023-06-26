efe997c9b360fbb94b505a0ca17c2e0137aefc67
Lema25 <56377966+Lema25@users.noreply.github.com>
2023-06-26 04:53:12 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -17,21 +17,14 @@ def clean_data():
     """
     df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
 
-    #
-    # Inserte su código aquí
-    #
-
     df.dropna(inplace=True)
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower();
-    df['sexo'] = df['sexo'].str.lower();
+    df.sexo = df.sexo.str.lower()
+    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
     df.idea_negocio = [str.lower(idea.replace("_", " ").replace("-", " ")) for idea in df.idea_negocio]
-    df.barrio = [str.lower(barrio).replace("_", " ").replace("-", " ") for barrio in df.barrio]
-    df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
-    df.estrato = df.estrato.astype(int)
-    df["línea_credito"] = [str.lower(linea.strip().replace("-", " ").replace("_", " ").replace(". ", ".")) for linea in df["línea_credito"]]
+    df.barrio = [str.lower(barrio).replace("_", " ").replace("-", " ") for barrio in df.barrio
+    df['línea_credito']=df['línea_credito'].apply(lambda x: x.lower().replace("-"," ").replace("_"," "))
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
     df['monto_del_credito']=df['monto_del_credito'].apply(lambda x: x.strip("$").replace(",","")).astype(float)
     df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(convertir_fecha)
-
-    df.drop_duplicates(inplace=True)
 
     return df
